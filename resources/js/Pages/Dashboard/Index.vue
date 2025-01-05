a6a0215939e5b010f7d9d1e9fd74404c2a4d95b4
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2025-01-05 15:46:48 +0700

    Update Vue

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">Trang Chủ</h1>
-    <p class="mb-8 leading-normal">Chào mừng đã đến với website quản lý nhân sự, đồ án năm 4 được code bởi <a class="text-indigo-500 underline hover:text-orange-600" href="https://siben.vn">Sĩ Ben</a></p>
+    <p class="mb-8 leading-normal">Chào mừng đã đến với website quản lý nhân sự</p>
   </div>
 </template>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ createInertiaApp({
   setup({ el, app, props }) {
     new Vue({
       metaInfo: {
-        titleTemplate: title => (title ? `${title} - siben.vn` : 'siben.vn'),
+        titleTemplate: title => (title ? `${title} - Phgmnhd` : 'Phgmnhd'),
       },
       render: h => h(app, props),
     }).$mount(el)
